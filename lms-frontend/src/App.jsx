edbf6b8d2b939d2ecbeb6b21feb8d37c1351dc9e
Mahendra Kumar <mahendra.mk174@gmail.com>
2025-04-17 12:29:54 +0530

    Dashboard fix

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Courses from './pages/Courses';
+import Dashboard from '../src/components/Dashboard/Dashboard';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <main className="centered-content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/courses" element={<Courses />} />

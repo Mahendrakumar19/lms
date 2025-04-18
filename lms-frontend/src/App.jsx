ea07442a06968248263126c65c9762f1ae43a93a
Mahendra Kumar <mahendra.mk174@gmail.com>
2025-04-18 11:57:41 +0530

    navbar code fix

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -1,28 +1,27 @@
 // App.jsx
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import Home from './pages/Home';
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home";
 // import About from './pages/About';
 // import Contact from './pages/Contact';
-import Courses from './pages/Courses';
-<<<<<<< HEAD
+import Courses from "./pages/Courses";
+
 // import DasBoard from './pages/DasBoard';
-=======
 
-
->>>>>>> 309769641921e96c4554421e004bc12f3b7a82e2
 function App() {
   return (
-    <div className='app-container'> 
+    <div className="app-container">
       <Navbar />
       <main className="centered-content">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/courses" element={<Courses />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/" element={<Courses />} />
+          {/* <Route path="/dashboard" element={<DasBoard />} /> */}
+          
         </Routes>
       </main>
       <Footer />
